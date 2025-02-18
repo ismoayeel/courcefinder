94b159801be15ca541d889e17e202d36652aa5dd
muxriddin229 <mavlonovm229@gmail.com>
2025-02-18 11:57:26 +0500

    qwerewq

--- config/db.js
+++ config/db.js
@@ -1,22 +1,12 @@
 import { Sequelize } from "sequelize";
 
 let sequelize = new Sequelize({
-<<<<<<< HEAD
   username: "root",
   password: "2004",
   host: "localhost",
-  database: "cource",
+  database: "coursefinder",
   dialect: "mysql",
   logging: false,
 });
-=======
-    username: "root",
-    password: "ismoayeel",
-    host: "localhost",
-    database: "coursefinder",
-    dialect: "mysql",
-    logging: false
-})
->>>>>>> fc7326ba67f79d08a624140564751d3efa720218
 
 export default sequelize;
