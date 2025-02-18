478b5c6ee833b2e08f7fa8099ee656f2fc17a976
ismoayeel <ergashboyevismoil922@gmail.comssh-keygen -o -t rsa -C windows-ssh@mcnz.com>
2025-02-18 11:55:10 +0500

    asfasf

--- config/db.js
+++ config/db.js
@@ -1,15 +1,6 @@
 import { Sequelize } from "sequelize";
 
 let sequelize = new Sequelize({
-<<<<<<< HEAD
-  username: "root",
-  password: "2004",
-  host: "localhost",
-  database: "cource",
-  dialect: "mysql",
-  logging: false,
-});
-=======
     username: "root",
     password: "ismoayeel",
     host: "localhost",
@@ -17,6 +8,5 @@ let sequelize = new Sequelize({
     dialect: "mysql",
     logging: false
 })
->>>>>>> fc7326ba67f79d08a624140564751d3efa720218
 
 export default sequelize;
